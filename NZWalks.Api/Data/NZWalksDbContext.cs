2b9564fc6e37f3380dc5fd37704c824e7c71219e
Kingswithkings <kingsuthanaidogu@gmail.com>
2024-01-01 13:22:57 +0100

    Add project files.

---
+++ NZWalks.Api/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using NZWalks.Api.Models;
+using NZWalks.Api.Models.Domain;
+
+namespace NZWalks.Api.Data
+{
+    public class NZWalksDbContext : DbContext 
+    {
+        public NZWalksDbContext(DbContextOptions dbContextoptions) : base(dbContextoptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> difficulties { get; set; }
+        public DbSet<Region> regions { get; set; }
+        public DbSet<Walk> walks { get; set; }
+
+    }
+}
