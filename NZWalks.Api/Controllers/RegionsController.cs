e4360fb382a4a0ba434bba083775eef3da98b2ac
Kingswithkings <kingsuthanaidogu@gmail.com>
2024-01-08 16:02:23 +0100

    making it Async method for the regionController
    Adding an Repository to create a CRUD

--- NZWalks.Api/Controllers/RegionsController.cs
+++ NZWalks.Api/Controllers/RegionsController.cs
@@ -17,33 +17,33 @@ namespace NZWalks.Api.Controllers
             this.dbContext = dbContext;
         }
 
-        //[HttpGet]
-        //public IActionResult GetAll()
-        //{
-        //    var regions = new List<Region>
+        /*[HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
 
-        //    {
-        //        new Region
-        //        {
-        //            Id = Guid.NewGuid(),
-        //            Name = "Auckland Region",
-        //            Code = "AKL",
-        //            RegionImageUrl = "https://images.pixel.com"
-        //        },
-        //        new Region
-        //        {
-        //            Id = Guid.NewGuid(), // Added missing comma and corrected Guid.NewGuid syntax
-        //            Name = "Wellington Region",
-        //            Code = "WLG",
-        //            RegionImageUrl = "http://images.pixels.com" // Corrected the protocol to "http"
-        //        }
-        //    };
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pixel.com"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(), // Added missing comma and corrected Guid.NewGuid syntax
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "http://images.pixels.com" // Corrected the protocol to "http"
+                }
+            };
 
-        //    return Ok(regions);
-        //}
+           return Ok(regions);
+        }*/
 
-        //GET ALL REGIONS
-        //GET: https://localhost:portnumber/api/regions
+        /*//GET ALL REGIONS     
+        //GET:https://localhost:portnumber/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
@@ -65,9 +65,36 @@ namespace NZWalks.Api.Controllers
 
             // Return DTOs
             return Ok(regionsDto);
+        }*/
+
+
+        //GET ALL REGIONS     
+        //GET:https://localhost:portnumber/api/regions
+        [HttpGet]
+        public async Task <IActionResult> GetAll()
+        {
+            // Get Data from Database - Domain Models
+            var regionsDomain = await dbContext.Regions.ToListAsync();
+
+            // Map Domain Models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var region in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl,
+                });
+            }
+
+            // Return DTOs
+            return Ok(regionsDto);
         }
 
-        // GET SINGLE REGION (Get Region By ID)
+
+        /*// GET SINGLE REGION (Get Region By ID)
         // GET: https://localhost:Portnumber/api/regions/{id}
         [HttpGet]
         [Route("{id:Guid}")]
@@ -83,7 +110,25 @@ namespace NZWalks.Api.Controllers
             }
 
             return Ok(region); // 200 OK with the region data as the response
+        }*/
+        // GET SINGLE REGION (Get Region By ID)
+        // GET: https://localhost:Portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task <IActionResult> GetById(Guid id)
+        {
+            //var region = dbContext.regions.Find(id);
+            // or
+            var region = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound(); // 404 Not Found when the region with the specified ID is not found
+            }
+
+            return Ok(region); // 200 OK with the region data as the response
         }
+
         /*The find method only takes the primary key so you can't use it to find cod or other properties
         *UNDERSTANDING DTOs and DOMAIN MODELs DTOs
         Used to transfer data between differnt Layers
@@ -94,7 +139,7 @@ namespace NZWalks.Api.Controllers
         Advantages of DTOs
         *Seperation of concerns *Performance *Security *Versoning
         *Change Method to Use Dto in model folder cretae a new folder DTO with a class RegionDto.cs*/
-        //Post To Create New Region
+        /*//Post To Create New Region
         //Post:https://localhost:portnumber/api/regions
         [HttpPost]
         public IActionResult Create([FromBody] AddRegionRequestDto  addRegionRequestDto)
@@ -118,9 +163,35 @@ namespace NZWalks.Api.Controllers
                 RegionImageUrl = regionDomainModel.RegionImageUrl,
             };
             return CreatedAtAction(nameof(GetById), new {id = regionDto.Id}, regionDto);
+        }*/
+        //Post To Create New Region
+        //Post:https://localhost:portnumber/api/regions
+        [HttpPost]
+        public async Task <IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            //Map or Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl,
+            };
+
+            //Use Domain Model to Create Region
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+            //Map Domain Model Back To DTO
+            var regionDto = new RegionDto
+            {
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl,
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
         }
 
-        //Update region
+
+        /*//Update region
         //Put:https:localhost:portnumber/api/regions/{id}
         [HttpPut]
         [Route("{id:Guid}")]
@@ -154,7 +225,43 @@ namespace NZWalks.Api.Controllers
             };
 
             return Ok(regionDto);
+        }*/
+        //Update region
+        //Put:https:localhost:portnumber/api/regions/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task <IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            // Check if region exists
+            var regionDomainModel = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            // If region does not exist, return NotFound
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Map DTO to Domain Model
+            // Assuming that updateRegionRequestDto.Id is not allowed to change
+            regionDomainModel.Code = updateRegionRequestDto.Code;
+            regionDomainModel.Name = updateRegionRequestDto.Name;
+            regionDomainModel.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+            // Save changes to the database
+            dbContext.SaveChanges();
+
+            // Convert Domain Model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Name = regionDomainModel.Name,
+                Code = regionDomainModel.Code,
+                RegionImageUrl = regionDomainModel.RegionImageUrl,
+            };
+
+            return Ok(regionDto);
         }
+
 
         //Delete Region
         //Delete:https://localhost:portnumber/api/regions/{id}
